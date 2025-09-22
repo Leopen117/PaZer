bae2061947e50eb1e01c394eceacb43fb9c865ca
Wilrun Schmidt <wilrun.schmidt@agentes.de>
2025-09-22 11:00:53 +0200

    button angelegt

--- frontend/pazer-frontend/src/App.tsx
+++ frontend/pazer-frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import { Banner } from "./components/banner";
+import { Button } from "./components/button";
 
 function App() {
   return (
     <>
       <Banner></Banner>
+      <Button></Button>
       {/* <HistoryChart></HistoryChart>
       
       <Input></Input>
---
+++ frontend/pazer-frontend/src/components/button.tsx
@@ -0,0 +1,20 @@
+export function Button() {
+    function buttonOnClickEvent() {
+
+        // pace berechnen und anzeigen
+       //double pace = zeit/km
+        // km,h und pace sollen in die Datenbank geschrieben werden
+            //warten auf Endpunkte
+        // Stockchart soll reloaded werden
+        // neuer Eintrag in der Historytabelle
+        }
+
+
+    return(
+        <>
+        <div className="button">
+           <button onClickEvent="buttonOnClickEvent()">Update</button>
+        </div>
+        </>
+        )
+    }
